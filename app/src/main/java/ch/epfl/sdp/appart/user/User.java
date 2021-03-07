8f7e942deb392a3f86606dbf37e088738ba0d710
CaiMusso <47753346+CaiMusso@users.noreply.github.com>
2021-03-07 19:01:44 +0100

    added general User interface
    All methods in this class are mandatory to implement for every user.
    
    note - the Object type was chosen for the getProfileImage() getter, should be changed once the object Picture is identified.

---
+++ app/src/main/java/ch/epfl/sdp/appart/user/User.java
@@ -0,0 +1,64 @@
+package ch.epfl.sdp.appart.user;
+
+public interface User {
+
+    /**
+     * @return the name of the user
+     */
+    public String getName();
+
+    /**
+     * sets the name of the user
+     */
+    public void setName();
+
+    /**
+     * getter method for ID
+     * @return the unique id of the user
+     */
+     public String getUserId();
+
+    /**
+     * setter method for user email
+     */
+    public void setUserEmail();
+
+    /**
+     * getter method for user email
+     * @return user email as a string
+     */
+     public String getUserEmail();
+
+    /**
+     * setter method for user phone number
+     */
+    public void setPhoneNumber();
+
+    /**
+     * getter method for user phone number
+     * @return user phone number as a string
+     */
+     public String getPhoneNumber();
+
+    /**
+     * setter method for user profile picture
+     */
+     public void setProfileImage();
+
+    /**
+     * getter method for user profile picture
+     * @return the profile picture (Object type was chosen as generic, should be changed once the object Picture is identified)
+     */
+    public Object getProfileImage();
+
+    /**
+     * send a verification email to the user
+     * @return true if email has been correctly sent, false otherwise
+     */
+    public boolean sendEmailVerification();
+
+    /**
+     * @return true if the user is registered with a university domain, false otherwise
+     */
+    public boolean hasUniversityEmail();
+}
