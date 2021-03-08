f950b07d7b26ff8a124118b62ee51c2f23cfd640
Antoine De Gendt <antoine.degendt@epfl.ch>
2021-03-08 21:24:10 +0100

    Remove the need of an executor for the FirebaseLoginService

--- app/src/main/java/ch/epfl/sdp/appart/login/FirebaseLoginService.java
+++ app/src/main/java/ch/epfl/sdp/appart/login/FirebaseLoginService.java
@@ -1,9 +1,5 @@
 package ch.epfl.sdp.appart.login;
 
-import android.content.Context;
-
-import androidx.core.content.ContextCompat;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
@@ -11,38 +7,22 @@ import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.util.concurrent.Executor;
-
 import ch.epfl.sdp.appart.user.User;
 
 public class FirebaseLoginService implements LoginService {
     private final FirebaseAuth mAuth;
-    private final Executor executor;
 
-    FirebaseLoginService(Executor executor) {
+    FirebaseLoginService() {
         this.mAuth = FirebaseAuth.getInstance();
-        this.executor = executor;
-    }
-
-    /**
-     * Gives an executor from the context to the FirebaseLoginService
-     *
-     * @param context the context from which we get an executor
-     * @return an executor
-     */
-    private static Executor buildExecutorFromContext(Context context) {
-        return ContextCompat.getMainExecutor(context);
     }
 
     /**
      * Builds a FirebaseLoginService from context
      *
-     * @param context the context needed to retrieve an executor
      * @return a FirebaseLoginService
      */
-    public static LoginService buildFromContext(Context context) {
-        if (context == null) throw new IllegalArgumentException();
-        return new FirebaseLoginService(buildExecutorFromContext(context));
+    public static LoginService buildLoginService() {
+        return new FirebaseLoginService();
     }
 
     @Override
@@ -50,7 +30,7 @@ public class FirebaseLoginService implements LoginService {
         if (email == null || password == null || callback == null)
             throw new IllegalArgumentException();
         mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this.executor, callback);
+                .addOnCompleteListener(callback);
     }
 
     @Override
@@ -71,7 +51,7 @@ public class FirebaseLoginService implements LoginService {
         if (email == null || password == null || callback == null)
             throw new IllegalArgumentException();
         mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this.executor, callback);
+                .addOnCompleteListener(callback);
     }
 
     @Override
