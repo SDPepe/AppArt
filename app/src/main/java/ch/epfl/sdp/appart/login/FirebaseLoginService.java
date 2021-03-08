ebdb1f0773e07446ea3526d3a58ab8dc994d02dd
Antoine De Gendt <antoine.degendt@epfl.ch>
2021-03-08 17:40:00 +0100

    Add useful functions and exceptions

--- app/src/main/java/ch/epfl/sdp/appart/login/FirebaseLoginService.java
+++ app/src/main/java/ch/epfl/sdp/appart/login/FirebaseLoginService.java
@@ -21,30 +21,6 @@ public class FirebaseLoginService implements LoginService {
         this.executor = executor;
     }
 
-    @Override
-    public void loginWithEmail(String email, String password, OnCompleteListener<AuthResult> callback) {
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this.executor, callback);
-    }
-
-    @Override
-    public User getCurrentUser() {
-        //Waiting for Adapter class
-        return null;
-    }
-
-    @Override
-    public void resetPasswordWithEmail(String email, OnCompleteListener<Void> callback) {
-        this.mAuth.sendPasswordResetEmail(email)
-                .addOnCompleteListener(callback);
-    }
-
-    @Override
-    public void createUser(String email, String password, OnCompleteListener<AuthResult> callback) {
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this.executor, callback);
-    }
-
     /**
      * Gives an executor from the context to the FirebaseLoginService
      *
@@ -62,6 +38,81 @@ public class FirebaseLoginService implements LoginService {
      * @return a FirebaseLoginService
      */
     public static LoginService buildFromContext(Context context) {
+        if (context == null) throw new IllegalArgumentException();
         return new FirebaseLoginService(buildExecutorFromContext(context));
+    }
+
+    @Override
+    public void loginWithEmail(String email, String password, OnCompleteListener<AuthResult> callback) {
+        if (email == null || password == null || callback == null)
+            throw new IllegalArgumentException();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this.executor, callback);
+    }
+
+    @Override
+    public User getCurrentUser() {
+        //Waiting for Adapter class
+        return null;
+    }
+
+    @Override
+    public void resetPasswordWithEmail(String email, OnCompleteListener<Void> callback) {
+        if (email == null || callback == null) throw new IllegalArgumentException();
+        this.mAuth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(callback);
+    }
+
+    @Override
+    public void createUser(String email, String password, OnCompleteListener<AuthResult> callback) {
+        if (email == null || password == null || callback == null)
+            throw new IllegalArgumentException();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this.executor, callback);
+    }
+
+    @Override
+    public void updateEmailAddress(User user, OnCompleteListener<Void> callback) {
+        if (user == null || callback == null) throw new IllegalArgumentException();
+        //We would need something like user.getLoginServiceUser
+        //which would return the appropriate user type for the LoginService through which the user was logged
+        //I have currently no idea on how to implement this
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void updatePassword(User user, OnCompleteListener<Void> callback) {
+        if (user == null || callback == null) throw new IllegalArgumentException();
+        //We would need something like user.getLoginServiceUser
+        //which would return the appropriate user type for the LoginService through which the user was logged
+        //I have currently no idea on how to implement this
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void sendEmailVerification(User user, OnCompleteListener<Void> callback) {
+        if (user == null || callback == null) throw new IllegalArgumentException();
+        //We would need something like user.getLoginServiceUser
+        //which would return the appropriate user type for the LoginService through which the user was logged
+        //I have currently no idea on how to implement this
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void deleteUser(User user, OnCompleteListener<Void> callback) {
+        if (user == null || callback == null) throw new IllegalArgumentException();
+        //We would need something like user.getLoginServiceUser
+        //which would return the appropriate user type for the LoginService through which the user was logged
+        //I have currently no idea on how to implement this
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void reAuthenticateUser(User user, OnCompleteListener<Void> callback) {
+        if (user == null || callback == null) throw new IllegalArgumentException();
+        //We would need something like user.getLoginServiceUser
+        //which would return the appropriate user type for the LoginService through which the user was logged
+        //I have currently no idea on how to implement this
+        throw new UnsupportedOperationException();
     }
 }
--- app/src/main/java/ch/epfl/sdp/appart/login/LoginService.java
+++ app/src/main/java/ch/epfl/sdp/appart/login/LoginService.java
@@ -43,4 +43,44 @@ public interface LoginService {
      * @param callback what should be done after success or failure
      */
     void createUser(String email, String password, OnCompleteListener<AuthResult> callback);
+
+    /**
+     * Updates the email address of a user
+     *
+     * @param user     the user who's email address mus be updated
+     * @param callback what is performed on update
+     */
+    void updateEmailAddress(User user, OnCompleteListener<Void> callback);
+
+    /**
+     * Updates the password of a user
+     *
+     * @param user     the user who's email address must be updated
+     * @param callback what is performed on password change if it succeeds or on failure
+     */
+    void updatePassword(User user, OnCompleteListener<Void> callback);
+
+    /**
+     * Verifies an user's email
+     *
+     * @param user     the user who's email needs to be updated
+     * @param callback what is performed on send success or failure
+     */
+    void sendEmailVerification(User user, OnCompleteListener<Void> callback);
+
+    /**
+     * Deletes a user
+     *
+     * @param user     the user we want to delete
+     * @param callback what is performed on deletion success or failure
+     */
+    void deleteUser(User user, OnCompleteListener<Void> callback);
+
+    /**
+     * Re-authenticates a user. This is needed for things like password change or critical operations
+     *
+     * @param user     the user we want to re-authenticate
+     * @param callback what is performed on re-authentication success or failure
+     */
+    void reAuthenticateUser(User user, OnCompleteListener<Void> callback);
 }
