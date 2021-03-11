3edb9655e559d292ca6e6bcf844c38399656bea1
rovati <lorenzo.rovati@tutanota.com>
2021-03-11 15:17:08 +0100

    Add SCrollingViewModel, modify ScrollingActivity to use VM

--- app/src/main/java/ch/epfl/sdp/appart/scrolling/ScrollingActivity.java
+++ app/src/main/java/ch/epfl/sdp/appart/scrolling/ScrollingActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.Arrays;
@@ -11,32 +12,28 @@ import java.util.List;
 
 import ch.epfl.sdp.appart.Card;
 import ch.epfl.sdp.appart.R;
+import ch.epfl.sdp.appart.scrolling.card.ApartmentCard;
 import ch.epfl.sdp.appart.scrolling.card.ApartmentCardAdapter;
-import ch.epfl.sdp.appart.user.AppUser;
-import ch.epfl.sdp.appart.user.User;
 
 public class ScrollingActivity extends AppCompatActivity {
+
+    private ScrollingViewModel mViewModel;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
 
-        User u = new AppUser("dummy", "dummy", "dummy", "dummy");
+        mViewModel = new ViewModelProvider(this).get(ScrollingViewModel.class);
 
-        List<Card> cardsId = Arrays.asList(
-                new Card(null, u, "Lausanne", 1000, "apart_fake_image_1.jpeg"),
-                new Card(null, u, "Ecublens", 1200, "apart_fake_image_2.jpeg"),
-                new Card(null, u, "Renens", 900, "apart_fake_image_3.jpeg"),
-                new Card(null, u, "Prilly", 1150, "apart_fake_image_4.jpeg"),
-                new Card(null, u, "Lausanne", 1200, "apart_fake_image_5.jpeg")
-        );
+        mViewModel.initHome();
 
+        recyclerView = findViewById(R.id.recycler_view);
 
-
-        RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        recyclerView.setAdapter(new ApartmentCardAdapter(this, cardsId));
         recyclerView.setHasFixedSize(true); //use for performance if card dims does not change
+
+        mViewModel.getCards().observe(this, this::updateList);
 
     }
 
@@ -44,6 +41,10 @@ public class ScrollingActivity extends AppCompatActivity {
     public void onStop() {
         super.onStop();
         Log.d("Scroll", "stoping");
+    }
+
+    private void updateList(List<Card> ls){
+        recyclerView.setAdapter(new ApartmentCardAdapter(this, ls));
     }
 
 }
