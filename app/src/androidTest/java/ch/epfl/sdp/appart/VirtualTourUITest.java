9020f8aa2ed3f952201683bc023f96dc9b36e001
Lorenzo Rovati <53517598+rovati@users.noreply.github.com>
2021-03-18 13:52:07 +0100

    Ass tests for VirtualTourActivity and contact info dialog

---
+++ app/src/androidTest/java/ch/epfl/sdp/appart/VirtualTourUITest.java
@@ -0,0 +1,42 @@
+package ch.epfl.sdp.appart;
+
+import android.app.Activity;
+
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import ch.epfl.sdp.appart.vtour.VirtualTourActivity;
+import dagger.hilt.android.testing.HiltAndroidRule;
+import dagger.hilt.android.testing.HiltAndroidTest;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertEquals;
+
+@HiltAndroidTest
+public class VirtualTourUITest {
+
+    @Rule(order = 0)
+    public HiltAndroidRule hiltRule  = new HiltAndroidRule(this);
+
+    @Rule(order = 1)
+    public ActivityScenarioRule vtourActivityRule =
+            new ActivityScenarioRule<>(VirtualTourActivity.class);
+
+    @Before
+    public void init() {
+        hiltRule.inject();
+    }
+
+    @Test
+    public void backButtonClosesActivity(){
+        onView(withId(R.id.tourBackButton)).perform(click());
+        assertEquals(Activity.RESULT_CANCELED, vtourActivityRule.getScenario().getResult()
+                .getResultCode());
+    }
+}
--- app/src/androidTest/java/ch/epfl/sdp/appart/ui/scrolling/AdUITest.java
+++ app/src/androidTest/java/ch/epfl/sdp/appart/ui/scrolling/AdUITest.java
@@ -1,15 +1,8 @@
 package ch.epfl.sdp.appart.ui.scrolling;
 
-import android.view.View;
-
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -18,7 +11,6 @@ import ch.epfl.sdp.appart.Database;
 import ch.epfl.sdp.appart.MockDataBase;
 import ch.epfl.sdp.appart.R;
 import ch.epfl.sdp.appart.hilt.FireBaseModule;
-import ch.epfl.sdp.appart.scrolling.ScrollingActivity;
 import ch.epfl.sdp.appart.scrolling.ad.AnnounceActivity;
 import ch.epfl.sdp.appart.vtour.VirtualTourActivity;
 import dagger.hilt.android.testing.BindValue;
@@ -29,11 +21,14 @@ import dagger.hilt.android.testing.UninstallModules;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @UninstallModules(FireBaseModule.class)
 @HiltAndroidTest
@@ -62,9 +57,13 @@ public class AdUITest {
     }
 
     @Test
-    public void clickOnInfoOpensContactDialog(){
+    public void contactDialogTests(){
         onView(withId(R.id.contactInfoButton)).perform(scrollTo()).perform(click());
-        onView(withId(R.id.dialogView)).check(matches(isDisplayed()));
+        onView(withText("Close"))
+                .inRoot(isDialog())
+                .check(matches(isDisplayed()))
+                .perform(click());
+        onView(withId(R.id.dialogView)).check(doesNotExist());
     }
 
 }
