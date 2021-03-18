a5f65b53b6c674ec64273b4ad9d4f753a3a54966
Lorenzo Rovati <53517598+rovati@users.noreply.github.com>
2021-03-18 12:32:21 +0100

    Fix tests

--- app/src/androidTest/java/ch/epfl/sdp/appart/ui/scrolling/AdUITest.java
+++ app/src/androidTest/java/ch/epfl/sdp/appart/ui/scrolling/AdUITest.java
@@ -18,6 +18,7 @@ import ch.epfl.sdp.appart.Database;
 import ch.epfl.sdp.appart.MockDataBase;
 import ch.epfl.sdp.appart.R;
 import ch.epfl.sdp.appart.hilt.FireBaseModule;
+import ch.epfl.sdp.appart.scrolling.ScrollingActivity;
 import ch.epfl.sdp.appart.scrolling.ad.AnnounceActivity;
 import ch.epfl.sdp.appart.vtour.VirtualTourActivity;
 import dagger.hilt.android.testing.BindValue;
@@ -27,6 +28,7 @@ import dagger.hilt.android.testing.UninstallModules;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
@@ -40,39 +42,11 @@ public class AdUITest {
     @Rule(order = 0)
     public HiltAndroidRule hiltRule  = new HiltAndroidRule(this);
 
-    /*@Rule(order = 1)
-    public ActivityScenario<AnnounceActivity> announceActivityRule =
-            new ActivityScenarioRule<>(AnnounceActivity.class);
-*/
+    @Rule(order = 1)
+    public ActivityScenarioRule scrollingActivityRule = new ActivityScenarioRule<>(AnnounceActivity.class);
+
     @BindValue
     Database database = new MockDataBase();
-
-    /**
-     * taken from :
-     * https://stackoverflow.com/questions/29378552/in-espresso-how-to-avoid-ambiguousviewmatcherexception-when-multiple-views-matc
-     * Allows to select the index th view.
-     *
-     * @param matcher the matcher on the view
-     * @param index   the index of the view we want to match
-     * @return a Matcher on the View
-     */
-    public static Matcher<View> withIndex(final Matcher<View> matcher, final int index) {
-        return new TypeSafeMatcher<View>() {
-            int currentIndex = 0;
-
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("with index: ");
-                description.appendValue(index);
-                matcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                return matcher.matches(view) && currentIndex++ == index;
-            }
-        };
-    }
 
     @Before
     public void init() {
@@ -89,7 +63,7 @@ public class AdUITest {
 
     @Test
     public void clickOnInfoOpensContactDialog(){
-        onView(withId(R.id.contactInfoButton)).perform(click());
+        onView(withId(R.id.contactInfoButton)).perform(scrollTo()).perform(click());
         onView(withId(R.id.dialogView)).check(matches(isDisplayed()));
     }
 
