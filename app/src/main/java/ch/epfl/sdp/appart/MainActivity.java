27f515c76786f7a553670a27fe6fa95392e5bf7b
Carlo Musso (Cai) <carlo.musso@epfl.ch>
2021-03-23 13:01:55 +0100

    restore LoginActivity as starting activity in Main, ready to merge

--- app/src/main/java/ch/epfl/sdp/appart/MainActivity.java
+++ app/src/main/java/ch/epfl/sdp/appart/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, UserProfileActivity.class);
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
 
     }
