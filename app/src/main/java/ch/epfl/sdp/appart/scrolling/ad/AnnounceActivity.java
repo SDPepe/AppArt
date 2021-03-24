cc632e437c309f31f718c91ebf8b0aaa1cbd2d38
Filippo Salmina <filippo.salmina@epfl.ch>
2021-03-24 20:50:01 +0100

    camera implemented

--- app/src/main/java/ch/epfl/sdp/appart/scrolling/ad/AnnounceActivity.java
+++ app/src/main/java/ch/epfl/sdp/appart/scrolling/ad/AnnounceActivity.java
@@ -17,6 +17,7 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
+import ch.epfl.sdp.appart.scrolling.CameraActivity;
 import com.bumptech.glide.Glide;
 
 import javax.inject.Inject;
@@ -89,6 +90,11 @@ public class AnnounceActivity extends AppCompatActivity {
 
     public void openVirtualTour(View view){
         Intent intent = new Intent(this, VirtualTourActivity.class);
+        startActivity(intent);
+    }
+
+    public void openCamera(View view){
+        Intent intent = new Intent(this, CameraActivity.class);
         startActivity(intent);
     }
 
