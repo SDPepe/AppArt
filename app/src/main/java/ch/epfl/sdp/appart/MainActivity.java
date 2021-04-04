328d0171b8b0eda86b0e82967525d204d5b61206
Carlo Musso (Cai) <carlo.musso@epfl.ch>
2021-04-04 13:17:50 +0200

    added espresso tests for SimpleUserProfileActivity

--- app/src/main/java/ch/epfl/sdp/appart/MainActivity.java
+++ app/src/main/java/ch/epfl/sdp/appart/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, LoginActivity.class);
+        Intent intent = new Intent(this, SimpleUserProfileActivity.class);
         startActivity(intent);
 
     }
