b69cdb8926ffe140f47a2f8bf1d9ea8e68cf5776
Antoine De Gendt <antoine.degendt@epfl.ch>
2021-04-15 19:30:28 +0200

    Trying another fix

--- app/src/androidTest/java/ch/epfl/sdp/appart/CameraUITest.java
+++ app/src/androidTest/java/ch/epfl/sdp/appart/CameraUITest.java
@@ -1,8 +1,14 @@
 package ch.epfl.sdp.appart;
 
+import android.view.View;
+
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
+import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -14,7 +20,11 @@ import dagger.hilt.android.testing.HiltAndroidTest;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
 
 @HiltAndroidTest
 public class CameraUITest {
@@ -34,12 +44,48 @@ public class CameraUITest {
 
     @Test
     public void clickOnCameraBtn() {
-        onView(withId(R.id.camera_Camera_button)).perform(scrollTo(), click());
+        //onView(withId(R.id.camera_Camera_button)).perform(click());
+        onView(withId(R.id.camera_Camera_button)).check(matches(allOf( isEnabled(), isClickable()))).perform(
+                new ViewAction() {
+                    @Override
+                    public Matcher<View> getConstraints() {
+                        return ViewMatchers.isEnabled(); // no constraints, they are checked above
+                    }
+
+                    @Override
+                    public String getDescription() {
+                        return "click camera button";
+                    }
+
+                    @Override
+                    public void perform(UiController uiController, View view) {
+                        view.performClick();
+                    }
+                }
+        );
     }
 
     @Test
     public void clickOnGalleryBtn() {
-        onView(withId(R.id.gallery_Camera_button)).perform(scrollTo(), click());
+        //onView(withId(R.id.gallery_Camera_button)).perform(click());
+        onView(withId(R.id.camera_Camera_button)).check(matches(allOf( isEnabled(), isClickable()))).perform(
+                new ViewAction() {
+                    @Override
+                    public Matcher<View> getConstraints() {
+                        return ViewMatchers.isEnabled(); // no constraints, they are checked above
+                    }
+
+                    @Override
+                    public String getDescription() {
+                        return "click gallery button";
+                    }
+
+                    @Override
+                    public void perform(UiController uiController, View view) {
+                        view.performClick();
+                    }
+                }
+        );
     }
 
     @After
