d6b52e8cd01107fbe035fd293dd44c84d6b20a5b
Carlo Musso (Cai) <carlo.musso@epfl.ch>
2021-04-28 23:58:43 +0200

    quick fix on MockDatabaseTest

--- app/src/test/java/ch/epfl/sdp/appart/MockDatabaseTest.java
+++ app/src/test/java/ch/epfl/sdp/appart/MockDatabaseTest.java
@@ -68,10 +68,10 @@ public class MockDatabaseTest {
         User user = new AppUser("1234", "test.appart@epfl.ch");
         assertTrue(dataBase.putUser(user).join());
         assertEquals(user, dataBase.getUser("1234").join());
-        assertTrue(dataBase.updateUser(user, mock(Uri.class)).join());
+        assertTrue(dataBase.updateUser(user).join());
         assertEquals(user, dataBase.getUser("1234").join());
         User user3 = new AppUser("4321", "test.appart@epfl.ch");
-        assertFalse(dataBase.updateUser(user3, mock(Uri.class)).join());
+        assertFalse(dataBase.updateUser(user3).join());
     }
 
     @Test
