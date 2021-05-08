25f261e158aaa2c9afcd632a9d42b13d12b3f066
Antoine De Gendt <antoine.degendt@epfl.ch>
2021-05-08 09:49:25 +0200

    Improve localAd by having a more relevant API, and fix path issues

--- app/src/main/java/ch/epfl/sdp/appart/database/local/LocalAd.java
+++ app/src/main/java/ch/epfl/sdp/appart/database/local/LocalAd.java
@@ -1,5 +1,7 @@
 package ch.epfl.sdp.appart.database.local;
 
+import android.util.Pair;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -7,12 +9,14 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 
 import ch.epfl.sdp.appart.ad.Ad;
+import ch.epfl.sdp.appart.database.firebaselayout.FirebaseLayout;
 import ch.epfl.sdp.appart.scrolling.card.Card;
 import ch.epfl.sdp.appart.user.AppUser;
 import ch.epfl.sdp.appart.user.User;
@@ -23,6 +27,61 @@ import ch.epfl.sdp.appart.utils.serializers.UserSerializer;
 public class LocalAd {
 
     private static final String ID = "ID";
+    private static final String favoritesFolder = "/favorites";
+    private static final String profilePicName = "user_profile_pic.jpg";
+    private static final String dataFileName = "ad.fav";
+
+    private static boolean createFolder(String adFolderPath, CompletableFuture<Void> futureSuccess) {
+        //Creating local folder for the complete ad
+        File adFolder = new File(adFolderPath);
+        if (!adFolder.exists()) {
+            boolean createdDir = adFolder.mkdirs();
+            if (!createdDir) {
+                futureSuccess.completeExceptionally(new IOException("Could " +
+                        "not create dir :" + adFolderPath));
+                return  false;
+            }
+        }
+        return true;
+    }
+
+    private static List<String> buildPhotoRefs(String adFolderPath, int size) {
+        List<String> localPhotoRefs = new ArrayList<>();
+        for (int i = 0; i < size; ++i) {
+            localPhotoRefs.add(adFolderPath + FirebaseLayout.SEPARATOR + FirebaseLayout.PHOTO_NAME + i + ".jpg");
+        }
+        return localPhotoRefs;
+    }
+
+    private static Pair<Ad, User> buildLocalStructures(Ad ad, User user, List<String> localPhotoRefs, String adFolderPath) {
+        Ad localAd = new Ad(ad.getTitle(), ad.getPrice(), ad.getPricePeriod()
+                , ad.getStreet(), ad.getCity(), ad.getAdvertiserId(),
+                ad.getDescription(), localPhotoRefs, ad.hasVRTour());
+
+        User localUser = new AppUser(user.getUserId(), user.getUserEmail());
+        localUser.setPhoneNumber(user.getPhoneNumber());
+        localUser.setName(user.getName());
+        localUser.setAge(user.getAge());
+        localUser.setGender(user.getGender());
+        localUser.setProfileImage(adFolderPath + FirebaseLayout.SEPARATOR + profilePicName);
+
+        return new Pair<>(localAd, user);
+    }
+
+    private static boolean writeFile(String adFolderPath, List<Map<String, Object>> mapList, CompletableFuture<Void> futureSuccess) {
+        FileOutputStream fos;
+        try {
+            fos = new FileOutputStream(adFolderPath + FirebaseLayout.SEPARATOR + dataFileName);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(mapList);
+            oos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            futureSuccess.completeExceptionally(e);
+            return false;
+        }
+        return true;
+    }
 
     /**
      * This function handles the writing of a complete ad to the local
@@ -40,7 +99,7 @@ public class LocalAd {
      * path to the images so that on load, the images from local storage will
      * be displayed.
      *
-     * @param card       the card that will written on disk
+     * @param cardId     the card id
      * @param ad         the ad that will be written on disk
      * @param user       the user that will be written on disk
      * @param appPath    the path of the app folder. It is given to the
@@ -54,7 +113,7 @@ public class LocalAd {
      * @return a Void completable future to indicate whether the task has
      * succeeded or not.
      */
-    public static CompletableFuture<Void> writeCompleteAd(Card card, Ad ad,
+    public static CompletableFuture<Void> writeCompleteAd(String cardId, Ad ad,
                                                           User user,
                                                           String appPath,
                                                           Function<String,
@@ -62,67 +121,39 @@ public class LocalAd {
 
         CompletableFuture<Void> futureSuccess = new CompletableFuture<>();
 
-        //Creating local folder for the complete ad
-        String adFolderPath = appPath + "/" + card.getId();
+        String favoritesPath = appPath + favoritesFolder + "/";
+        String adFolderPath = favoritesPath + cardId;
 
-        File adFolder = new File(adFolderPath);
-        if (!adFolder.exists()) {
-            boolean createdDir = adFolder.mkdir();
-            if (!createdDir) {
-                futureSuccess.completeExceptionally(new IOException("Could " +
-                        "not create dir :" + adFolderPath));
-            }
-        }
+        if(!createFolder(adFolderPath, futureSuccess)) return futureSuccess;
+
+
+        //I think we do not need to do something specific if the folder already exists, we will just overwrite the files
+        //Maybe we need to check for the number of images so that if the ad has one image less than before we do not could keep a stale image in the folder
 
         //Changing the photoRefs to local paths
-        List<String> localPhotoRefs = new ArrayList<>();
-        for (int i = 0; i < ad.getPhotosRefs().size(); ++i) {
-            localPhotoRefs.add(appPath + "/" + "Photo" + i + ".jpg");
-        }
+        List<String> localPhotoRefs = buildPhotoRefs(adFolderPath, ad.getPhotosRefs().size());
 
-        //Building local versions of the ad and card
-        Card localCard = new Card(card.getId(), card.getAdId(),
-                card.getUserId(), card.getCity(), card.getPrice(),
-                card.getImageUrl(), card.hasVRTour());
-        Ad localAd = new Ad(ad.getTitle(), ad.getPrice(), ad.getPricePeriod()
-                , ad.getStreet(), ad.getCity(), ad.getAdvertiserId(),
-                ad.getDescription(), localPhotoRefs, ad.hasVRTour());
+        //Building local versions of the ad only because we can build the card from the ad
+        Pair<Ad, User> structures = buildLocalStructures(ad, user, localPhotoRefs, adFolderPath);
 
-        User localUser = new AppUser(user.getUserId(), user.getUserEmail());
-        localUser.setPhoneNumber(user.getPhoneNumber());
-        localUser.setName(user.getName());
-        localUser.setAge(user.getAge());
-        localUser.setGender(user.getGender());
-        localUser.setProfileImage(appPath + "/user_profile_pic.jpg");
-
-
+        Ad localAd = structures.first;
+        User localUser = structures.second;
 
         //Serializing
-        Map<String, Object> cardMap = CardSerializer.serialize(localCard);
-        cardMap.put(ID, localCard.getId());
-
         Map<String, Object> adMap = AdSerializer.serialize(localAd);
 
-
         Map<String, Object> userMap = UserSerializer.serialize(localUser);
+        //We ad the id because it is not serialized
         userMap.put(ID, localUser.getUserId());
 
+        //We use a list of HashMap so that it is simple to put all the data in a single file.
+        //Indeed, we can serialize the List as one big object and then retrieve the individual hash maps
         List<Map<String, Object>> mapList = new ArrayList<>();
-        mapList.add(cardMap);
         mapList.add(adMap);
         mapList.add(userMap);
 
-
-        FileOutputStream fos;
-        try {
-            fos = new FileOutputStream(appPath);
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-            oos.writeObject(mapList);
-            oos.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            futureSuccess.completeExceptionally(e);
-        }
+        //Write file on disk
+        if(!writeFile(adFolderPath, mapList, futureSuccess)) return futureSuccess;
 
         CompletableFuture<Void> futureImageLoad =
                 loadImages.apply(adFolderPath);
@@ -135,7 +166,7 @@ public class LocalAd {
         return futureSuccess;
     }
 
-    public static CompletableFuture<LocalCompleteAd> loadCompleteAd(String fullPath) {
+    /*public static CompletableFuture<LocalCompleteAd> loadCompleteAd(String fullPath) {
         CompletableFuture<LocalCompleteAd> futureCompleteAd =
                 new CompletableFuture<>();
 
@@ -167,6 +198,10 @@ public class LocalAd {
         futureCompleteAd.complete(completeAd);
 
         return futureCompleteAd;
+    }*/
+
+    public static CompletableFuture<List<LocalCompleteAd>> findLocalAds(String appFolder) {
+        return  null;
     }
 
     public static class LocalCompleteAd {
@@ -180,4 +215,5 @@ public class LocalAd {
             this.user = user;
         }
     }
+
 }
