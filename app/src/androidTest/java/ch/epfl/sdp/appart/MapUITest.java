3fef0d4c4610481b54f1ce936931df86b074b750
Antoine De Gendt <antoine.degendt@epfl.ch>
2021-05-10 12:15:16 +0200

    Remove unused function

--- app/src/androidTest/java/ch/epfl/sdp/appart/MapUITest.java
+++ app/src/androidTest/java/ch/epfl/sdp/appart/MapUITest.java
@@ -148,26 +148,6 @@ public class MapUITest {
         Intents.init();
     }
 
-    public static ViewAction clickXY(final int x, final int y){
-        return new GeneralClickAction(
-                Tap.SINGLE,
-                new CoordinatesProvider() {
-                    @Override
-                    public float[] calculateCoordinates(View view) {
-
-                        final int[] screenPos = new int[2];
-                        view.getLocationOnScreen(screenPos);
-
-                        final float screenX = screenPos[0] + x;
-                        final float screenY = screenPos[1] + y;
-                        float[] coordinates = {screenX, screenY};
-
-                        return coordinates;
-                    }
-                },
-                Press.FINGER);
-    }
-
     @Test
     public void markerClickTest() throws InterruptedException {
 
