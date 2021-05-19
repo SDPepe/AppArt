b42d4998e260b460881880a042b51c233e0b6170
Hbju <milou@ordonneau.com>
2021-05-19 19:37:07 +0200

    fixed tests

--- app/src/androidTest/java/ch/epfl/sdp/appart/UserAdsActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/appart/UserAdsActivityTest.java
@@ -35,15 +35,17 @@ import dagger.hilt.android.testing.UninstallModules;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static ch.epfl.sdp.appart.ViewUtils.withIndex;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
 
 @UninstallModules({DatabaseModule.class, LoginModule.class})
 @HiltAndroidTest
@@ -67,63 +69,35 @@ public class UserAdsActivityTest {
         hiltRule.inject();
     }
 
-
-    /**
-     * taken from :
-     * https://stackoverflow.com/questions/29378552/in-espresso-how-to-avoid-ambiguousviewmatcherexception-when-multiple-views-matc
-     * Allows to select the index th view.
-     *
-     * @param matcher the matcher on the view
-     * @param index   the index of the view we want to match
-     * @return a Matcher on the View
-     */
-    private static Matcher<View> withIndex(final Matcher<View> matcher, final int index) {
-        return new TypeSafeMatcher<View>() {
-            int currentIndex = 0;
-
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("with index: ");
-                description.appendValue(index);
-                matcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                return matcher.matches(view) && currentIndex++ == index;
-            }
-        };
-    }
-
     @Test
     public void userThatCreatedAnAdCanFindHisAds() {
-        ViewInteraction appCompatEditText = onView(
+        ViewInteraction appCompatEditText6 = onView(
                 allOf(withId(R.id.email_Login_editText),
                         childAtPosition(
                                 childAtPosition(
                                         withId(android.R.id.content),
                                         0),
-                                2),
+                                0),
                         isDisplayed()));
-        appCompatEditText.perform(replaceText("emilien@epfl.ch"), closeSoftKeyboard());
+        appCompatEditText6.perform(replaceText("emilien@epfl.ch"), closeSoftKeyboard());
 
-        ViewInteraction appCompatEditText2 = onView(
+        ViewInteraction appCompatEditText11 = onView(
                 allOf(withId(R.id.password_Login_editText),
                         childAtPosition(
                                 childAtPosition(
                                         withId(android.R.id.content),
                                         0),
-                                3),
+                                1),
                         isDisplayed()));
-        appCompatEditText2.perform(replaceText("5555"), closeSoftKeyboard());
+        appCompatEditText11.perform(click(), replaceText("5555"), closeSoftKeyboard());
 
         ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.login_Login_button), withText("Login"),
+                allOf(withId(R.id.login_Login_button), withText("Log In!"),
                         childAtPosition(
                                 childAtPosition(
                                         withId(android.R.id.content),
                                         0),
-                                0),
+                                2),
                         isDisplayed()));
         appCompatButton.perform(click());
 
@@ -147,10 +121,8 @@ public class UserAdsActivityTest {
                         isDisplayed()));
         appCompatTextView.perform(click());
 
-        ViewInteraction imageView = onView(withIndex(withId(R.id.image_CardLayout_imageView), 0));
-        imageView.check(matches(isDisplayed()));
+        onView(allOf(withIndex(withId(R.id.image_CardLayout_imageView), 0),isDisplayed()));
     }
-
 
     private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
