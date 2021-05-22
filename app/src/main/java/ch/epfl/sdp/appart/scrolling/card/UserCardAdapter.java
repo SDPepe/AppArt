68fba283e7feb5c37b69f8d41937176fc3d1f184
Hbju <milou@ordonneau.com>
2021-05-22 17:58:34 +0200

    minor changes

---
+++ app/src/main/java/ch/epfl/sdp/appart/scrolling/card/UserCardAdapter.java
@@ -0,0 +1,28 @@
+package ch.epfl.sdp.appart.scrolling.card;
+
+
+import android.app.Activity;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import ch.epfl.sdp.appart.database.DatabaseService;
+
+/**
+ * Adapter converting a card created by the current user into a CardViewHolder for the UserAdsActivity
+ * Main difference with CardAdapter is the presence of two buttons, to edit and delete the ad.
+ */
+public class UserCardAdapter extends CardAdapter {
+    List<Card> cards;
+
+    public UserCardAdapter(Activity context, DatabaseService databaseService, List<Card> cards) {
+        super(context, databaseService, cards);
+
+        this.cards = cards;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CardViewHolder holder, int position) {
+        super.onBindViewHolder(holder, position);
+    }
+}
