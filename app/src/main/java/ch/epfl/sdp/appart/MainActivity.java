60b8c64835f7d085cce5b59e7b496d78970fdbfd
Antoine De Gendt <antoine.degendt@epfl.ch>
2021-05-28 09:12:03 +0200

    Fix tests

--- app/src/main/java/ch/epfl/sdp/appart/MainActivity.java
+++ app/src/main/java/ch/epfl/sdp/appart/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import ch.epfl.sdp.appart.login.LoginService;
 import ch.epfl.sdp.appart.utils.ActivityCommunicationLayout;
 
 import javax.inject.Inject;
@@ -27,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Bundle extras = this.getIntent().getExtras();
-        Intent intent = new Intent(this, PlaceActivity.class);
+        Intent intent = new Intent(this, LoginActivity.class);
         if(extras != null && extras.containsKey(ActivityCommunicationLayout.PROVIDING_EMAIL)  && extras.containsKey(ActivityCommunicationLayout.PROVIDING_PASSWORD)){
             intent.putExtra(ActivityCommunicationLayout.PROVIDING_EMAIL, extras.getString(ActivityCommunicationLayout.PROVIDING_EMAIL));
             intent.putExtra(ActivityCommunicationLayout.PROVIDING_PASSWORD, extras.getString(ActivityCommunicationLayout.PROVIDING_PASSWORD));
