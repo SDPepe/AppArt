5ed40a5c36cc252729f25001eaf430b02f8b1bb0
GrapixLeGrand <38352876+GrapixLeGrand@users.noreply.github.com>
2021-06-04 16:58:00 +0200

    fixed toolbar and add home button

--- app/src/main/java/ch/epfl/sdp/appart/UserAdsActivity.java
+++ app/src/main/java/ch/epfl/sdp/appart/UserAdsActivity.java
@@ -10,6 +10,10 @@ import ch.epfl.sdp.appart.ad.UserAdsViewModel;
 import dagger.hilt.android.AndroidEntryPoint;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,12 +28,23 @@ public class UserAdsActivity extends ToolbarActivity {
     private RecyclerView recyclerView;
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuItem item = menu.findItem(R.id.action_userAds);
+        item.setVisible(false);
+        invalidateOptionsMenu();
+        return true;
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_ads);
 
         Toolbar toolbar = findViewById(R.id.UserAds_toolbar);
         setSupportActionBar(toolbar);
+        //toolbar.getMenu().removeItem(R.id.action_userAds);
+
 
         UserAdsViewModel mViewModel = new ViewModelProvider(this).get(UserAdsViewModel.class);
 
